c0f9363f6a556b6ef985fbbb8467078918c64667
jimmoser <74271033+jimmoser@users.noreply.github.com>
2022-11-27 14:50:11 -0600

    updated trial similarity.py to push out a csv rather than json, and created similar_trials.html to make a d3 table, but it doesn't work.

--- trial_similarity/trial_similarity.py
+++ trial_similarity/trial_similarity.py
@@ -1,3 +1,4 @@
+import json
 import pandas as pd
 import numpy as np
 from sklearn.feature_extraction.text import TfidfVectorizer
@@ -24,15 +25,16 @@ A = cosine_similarity(X[selected_index], X)
 
 # Creates a df of the top n similarity scores
 # Since A is a nested array, we need to use the A[0] to get within it.
-df_sim_score = pd.DataFrame({'similarity_score': np.sort(A[0])[-top_n:]})
+# The last record is the selected record and isn't included
+df_sim_score = pd.DataFrame({'similarity_score': np.sort(A[0])[-(top_n+1):-1]})
 
 # Creates a df of the clinical trial records with  the top n similarity scores to
-# the 'selected' trial.
-df_top_trials = df.iloc[(np.argsort(A[0]))[-top_n:], :].reset_index(drop=True)
+# the 'selected' trial. The last record is the selected record and isn't included
+df_top_trials = df.iloc[(np.argsort(A[0]))[-(top_n+1):-1], :].reset_index(drop=True)
 
 # Concatenates the list of trials with the corresponding similiarity scores.
 df_top_w_scores = pd.concat([df_top_trials, df_sim_score], axis=1)
 
-print(df_top_w_scores)
-
+# Creates a csv file of the top similar results.
+df_top_w_scores.to_csv("top_similar.csv", index=False)
 
