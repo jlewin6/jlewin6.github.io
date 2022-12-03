31a9ddb76b1c42533a2bdb5c14fcefccfef728f2
eye7 <eye7@gatech.edu>
2022-12-03 12:42:13 -0500

    fixed nav

--- webpages/query.sql
+++ webpages/query.sql
@@ -6,13 +6,18 @@ t.name AS intervention,
 s.overall_status,
 a.name AS country,
 s.brief_title,
-b.description AS study_description
+b.description AS study_description,
+s.completion_date AS date
 FROM conditions c
 LEFT JOIN studies s USING (nct_id)
 LEFT JOIN countries a USING (nct_id)
 LEFT JOIN intervention_other_names i USING (nct_id)
 LEFT JOIN interventions t USING (nct_id)
 LEFT JOIN brief_summaries b USING (nct_id)
-WHERE a.name is not null AND i.intervention_id is not null AND intervention_type IN ('Drug') AND overall_status != 'Terminated' AND a.name IN ('United States')
+WHERE (a.name is not null)
+AND (i.intervention_id is not null)
+AND (t.name != 'Placebo')
+AND (overall_status != 'Terminated')
+AND (a.name IN ('United States'))
+AND (s.completion_date > '2022-01-01' )
 AND c.name IN ('Brain Tumors', 'Sarcoma', 'Breast Cancer', 'Leukemia', 'Pancreatic Cancer', 'Colorectal Cancer', 'Lymphoma', 'Head and Neck Cancer', 'Melanoma', 'Lung Cancer', 'Renal Cell Carcinoma', 'Multiple Myeloma', 'Esophageal Cancer', 'Adenocarcinoma', 'Ovarian Cancer' , 'Bladder Cancer', 'Liver Cancer')
-LIMIT 2000
